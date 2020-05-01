483c0270b9cf1db1b5abe00d9d2c21fe6ef2a62a
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-05-01 23:04:58 +0100

    Added landing page

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,34 +3,34 @@ import Bounce from "react-reveal/Bounce"
 import Rotate from "react-reveal/Rotate"
 import Image from "./image"
 import React from "react"
+import './intro.scss'
 
-import Im from "../images/overlay.svg"
+import Im1 from "../images/overlay.svg"
 
 const Intro = () => (
-    <div>
-        <div className="row" >
-            <div className="col-lg-5">
-                <Anim>
-                    <h1 className="main-heading" >Hi There!</h1>
-                </Anim>
-                <br />
-                <Anim>
-                    <h4 className="main-subheading" >I'm John and I'm a JAMStack developer</h4>
-                </Anim>
-                <br /><br />
-                <Bounce>
-                    <button className="btn rounded-pill hireButton" >Hire Me</button>
-                </Bounce>
-                <br /><br />
-            </div>
-            <div className="col-lg-7">
-                <Rotate>
+    <div className="mainIntro" >
+        <div className="container">
+            <div className="row" >
+                <div className="col-lg-5">
+                    <Anim>
+                        <h1 className="main-heading" >Hi There!</h1>
+                    </Anim>
+                    <br />
+                    <Anim>
+                        <h4 className="main-subheading" >I'm John and I'm a JAMStack developer</h4>
+                    </Anim>
+                    <br /><br />
+                    <Bounce>
+                        <button className="btn rounded-pill hireButton" >Hire Me</button>
+                    </Bounce>
+                    <br /><br />
+                </div>
+                <div className="col-lg-7">
                     <Image />
-
-                </Rotate>
+                </div>
             </div>
+            <br /><br />
         </div>
-        <br /><br />
     </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,13 @@ import React from "react"
 import '../components/index.scss'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Image from '../components/image'
-import Anim from "react-reveal/Zoom"
-import Bounce from "react-reveal/Bounce"
 import Intro from "../components/intro"
 
 const IndexPage = ({ data }) => (
     <Layout>
         <SEO title="Home" />
-        <div className="mainIntro">
-            <div className="container">
-                <Intro />
-                <hr /><br />
-            </div>
-        </div>
+        <Intro />
+        <br /><br /><br />
     </Layout>
 )
 
