487b1268ea2b058cebd4b0a1d0c6bd4e6f0e05d4
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-05-04 14:18:03 +0100

    Updated image alt

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Me from '../../images/john3.jpg'
 import './About.scss'
 import Skills from "../Skills/Skills"
 import {graphql, useStaticQuery} from 'gatsby'
@@ -28,7 +27,7 @@ const About = () => {
             <div className="row">
                 <div className="col-lg-6">
                     <br/><br/>
-                    <Image fluid={data.file.childImageSharp.fluid} alt="me"  className="img-fluid rounded about-image"/>
+                    <Image fluid={data.file.childImageSharp.fluid} alt="Oluwakeye John"  className="img-fluid rounded about-image"/>
                     <br/><br/>
                     <h4 className="text-muted">Who's this guy?</h4>
                     <br/>
@@ -51,18 +50,5 @@ const About = () => {
 }
 
 
-//     <h3 className="text-muted">About Myself</h3>
-// <br />
-// <div className="mr-4 text-muted about-key">
-//     <p>Name </p>
-//     <p>Address </p>
-//     <p>Hobbies </p>
-// </div>
-// <div className="about-value">
-//     <p>Oluwakeye John</p>
-//     <p>Ibadan, Nigeria</p>
-//     <p>Cracking jokes</p>
-// </div>
-// <br /><br />
-// <HireButton />
+
 export default About
